46beb1fa1aa9a2a638decaf9b64a73105eb5ca2c
Sugar_F0x <malion.torchwood@yandex.ru>
2020-09-22 22:01:32 +0300

    :white_check_mark: Add skeleton for masters display
    Masters will be displayed via Content markdown

---
+++ pages/staff/_master.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-content :document="master" />
+</template>
+
+<script lang="ts">
+export default {
+  name: "_master",
+
+  async asyncData({ error, $content, params }) {
+    try {
+      const master = await $content('staff', params.master).fetch()
+
+      return { master }
+    } catch (err) {
+      error({ statusCode: 404, message: 'Master not found' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
