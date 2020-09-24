ca6f9cd175c242c800c137d6589640b49571d8dc
Sugar_F0x <malion.torchwood@yandex.ru>
2020-09-24 11:44:10 +0300

    :art: Revert Logo placeholder text prop
    Highlighting it's borders as a button is more intuitive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar fixed short app>
-      <v-btn text href="/">here be logo</v-btn>
+      <v-btn href="/">here be logo</v-btn>
       <v-spacer/>
       <v-btn v-if="$auth.loggedIn" @click="$auth.logout('local')">Logout</v-btn>
     </v-app-bar>
