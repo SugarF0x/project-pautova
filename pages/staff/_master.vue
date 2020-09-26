725c17215a51d22ea0d1c5c3f034a2001f79d1e0
Sugar_F0x <malion.torchwood@yandex.ru>
2020-09-26 11:43:04 +0300

    :white_check_mark: Add simple form template

--- pages/staff/_master.vue
+++ pages/staff/_master.vue
@@ -1,5 +1,30 @@
 <template>
-  <nuxt-content :document="master" />
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="10">
+        <nuxt-content :document="master" />
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="10">
+        <v-form ref="form">
+          <v-card>
+            <v-card-title>Apply now</v-card-title>
+            <v-card-text class="form-text">
+              <div>Applying for: <b>{{ master.title }}</b></div>
+              <div>Name: <v-text-field></v-text-field></div>
+              <div>Comment: <v-text-field></v-text-field></div>
+              <div></div>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn @click="submit">Submit</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,10 +41,19 @@ export default Vue.extend({
     } catch (err) {
       context.error({ statusCode: 404, message: 'Master not found' })
     }
+  },
+
+  methods: {
+    submit() {
+      alert('Request sent');
+      this.$router.push('/');
+    }
   }
 })
 </script>
 
 <style lang="less" scoped>
-
+.form-text > * > * {
+  display: inline-block;
+}
 </style>
